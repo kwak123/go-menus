613ba8f43e745f0d79d28f89a9b6359c1f9ae028
kwak123 <kwak123@gmail.com>
2019-05-11 00:32:29 -0400

    use switch, extract json parsing

--- server.go
+++ server.go
@@ -17,24 +17,15 @@ func makeRouteHandler() http.HandlerFunc {
 	m := &db.Menu{ID: "123", Name: "Test"}
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		apiPrefix := "/api/"
-		pathWithoutAPIPrefix := r.URL.Path[len(apiPrefix):]
-
-		if r.Method == "GET" {
-			handleGetMenu(w, r, m)
-		} else if r.Method == "POST" {
-			switch pathWithoutAPIPrefix {
-			case "add":
-				handleAddItemToMenu(w, r, m)
-			case "delete":
-				handleDeleteItem(w, r, m)
-			default:
-				w.WriteHeader(400)
-			}
-		} else if r.Method == "PUT" {
-			handleUpdateItem(w, r, m)
-		} else {
-			fmt.Fprint(w, "Fai")
+		switch r.Method {
+		case "GET":
+			handleGetRequest(w, r, m)
+		case "POST":
+			handlePostRequest(w, r, m)
+		case "PUT":
+			handlePutRequest(w, r, m)
+		default:
+			fmt.Fprint(w, "Invalid request")
 		}
 	}
 }
@@ -46,7 +37,21 @@ func handleGetRequest(w http.ResponseWriter, r *http.Request, m *db.Menu) {
 
 // Only have one post for now
 func handlePostRequest(w http.ResponseWriter, r *http.Request, m *db.Menu) {
-	handleAddItemToMenu(w, r, m)
+	apiPrefix := "/api/"
+	pathWithoutAPIPrefix := r.URL.Path[len(apiPrefix):]
+
+	switch pathWithoutAPIPrefix {
+	case "add":
+		handleAddItemToMenu(w, r, m)
+	case "delete":
+		handleDeleteItem(w, r, m)
+	default:
+		w.WriteHeader(400)
+	}
+}
+
+func handlePutRequest(w http.ResponseWriter, r *http.Request, m *db.Menu) {
+	handleUpdateItem(w, r, m)
 }
 
 func handleGetMenu(w http.ResponseWriter, r *http.Request, m *db.Menu) {
@@ -58,6 +63,22 @@ func handleGetMenu(w http.ResponseWriter, r *http.Request, m *db.Menu) {
 	w.Write(menuJSON)
 }
 
+func parseBodyForJSON(w http.ResponseWriter, r *http.Request, v interface{}) error {
+	JSON, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error parsing body: %s", err)
+	}
+
+	err = json.Unmarshal(JSON, v)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error unmarshalling JSON")
+	}
+
+	return err
+}
+
 // TODO: Expand to add an item
 func handleAddItemToMenu(w http.ResponseWriter, r *http.Request, m *db.Menu) {
 	// TODO: Remove this mock id handler
@@ -65,17 +86,10 @@ func handleAddItemToMenu(w http.ResponseWriter, r *http.Request, m *db.Menu) {
 	// Initialize item
 	item := db.Item{ID: id}
 
-	// Try to read the body
-	itemJSON, err := ioutil.ReadAll(r.Body)
+	err := parseBodyForJSON(w, r, &item)
 
 	if err != nil {
-		fmt.Fprintf(w, "Error parsing body: %s", err)
-	}
-
-	err = json.Unmarshal(itemJSON, &item)
-
-	if err != nil {
-		fmt.Fprintf(w, "Error converting JSON to item: %s", err)
+		fmt.Fprint(w, "Failed to parse")
 	}
 
 	m.AddItem(item)
@@ -85,16 +99,11 @@ func handleAddItemToMenu(w http.ResponseWriter, r *http.Request, m *db.Menu) {
 // Only name, one thing at a time for now haha
 func handleUpdateItem(w http.ResponseWriter, r *http.Request, m *db.Menu) {
 	updateItem := db.Item{}
-	updateJSON, err := ioutil.ReadAll(r.Body)
+
+	err := parseBodyForJSON(w, r, &updateItem)
 
 	if err != nil {
-		fmt.Fprintf(w, "Error parsing body: %s", err)
-	}
-
-	err = json.Unmarshal(updateJSON, &updateItem)
-
-	if err != nil {
-		fmt.Fprintf(w, "Error converting JSON to item: %s", err)
+		fmt.Fprintf(w, "Failed to update item")
 	}
 
 	m.UpdateItem(updateItem.ID, updateItem.Name)
@@ -102,20 +111,15 @@ func handleUpdateItem(w http.ResponseWriter, r *http.Request, m *db.Menu) {
 }
 
 func handleDeleteItem(w http.ResponseWriter, r *http.Request, m *db.Menu) {
-	itemToDelete := db.Item{}
-	deleteJSON, err := ioutil.ReadAll(r.Body)
+	deleteItem := db.Item{}
+
+	err := parseBodyForJSON(w, r, &deleteItem)
 
 	if err != nil {
-		fmt.Fprintf(w, "Error parsing body: %s", err)
+		fmt.Fprintf(w, "Failed to delete item")
 	}
 
-	err = json.Unmarshal(deleteJSON, &itemToDelete)
-
-	if err != nil {
-		fmt.Fprintf(w, "Error converting JSON to item: %s", err)
-	}
-
-	m.DeleteItem(itemToDelete.ID)
+	m.DeleteItem(deleteItem.ID)
 	handleGetMenu(w, r, m)
 }
 
