138f7602fd48e40eeaa35ce8d05eebbfd2565cc8
kwak123 <kwak123@gmail.com>
2019-05-11 11:17:12 -0400

    connect to mongod

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,8 +1,52 @@
 package db
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var database *mongo.Database
+var hasInitialized = false
+
+// InitializeDb starts the MongoDB client
+func InitializeDb() {
+	if !hasInitialized {
+		client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+		if err != nil {
+			println("Failed to initialize db client: %s", err)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		err = client.Connect(ctx)
+
+		if err != nil {
+			println("Failed to connect to db: %s", err)
+		}
+
+		hasInitialized = true
+		println("Connected to db")
+		database = client.Database("test")
+		refreshDb()
+		cancel()
+	} else {
+		println("Already connected")
+	}
+}
+
+func refreshDb() {
+	err := database.Collection("menu").Drop(context.TODO())
+	if err != nil {
+		println("Failed to refresh db: %s", err)
+	} else {
+		println("DB refreshed")
+	}
+	database.Collection("menu")
+}
 
 var menu = Menu{ID: "123", Name: "Test"}
 
