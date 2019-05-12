19c4b0c85ae35167354a4c4db06a5ef08e33f861
kwak123 <kwak123@gmail.com>
2019-05-12 13:05:51 -0400

    client ui package

--- client/App.jsx
+++ client/App.jsx
@@ -5,13 +5,24 @@ import { Router, Link } from '@reach/router';
 import Main from './components/Main.jsx';
 import Menu from './components/menu/Menu';
 
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Main path="/" />
-        <Menu path="/app/:menuId" />
-      </Router>
+      <div>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              Go Menu!
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Router>
+          <Main path="/" />
+          <Menu path="/app/:menuId" />
+        </Router>
+      </div>
     );
   }
 }
