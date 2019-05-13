7f3a8eb50c082e5f8ed05d03842b51c515522984
kwak123 <kwak123@gmail.com>
2019-05-13 15:43:12 -0400

    style items

--- client/components/menu/Item.jsx
+++ client/components/menu/Item.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-import api from '../../api/api';
+import MenuContext from '../../contexts/menu';
 
 const Item = (props) => {
   const { item } = props;
   const { name, provider } = item;
 
   return (
-    <li>
-      <h3>Item: {name}</h3>
-      <p>Provider: {provider}</p>
-    </li>
+    <MenuContext.Consumer>
+      {({ updateMenuItem }) => (
+        <li className="menu-item" onBlur={() => console.log('test')}>
+          <div className="menu-item__name-field">
+            <p>Item: </p>
+            <input type="text" value={item.name}></input>
+          </div>
+          <div className="menu-item__provider-field">
+            <p>Provider: </p>
+            <input type="text" value={item.provider}></input>
+          </div>
+        </li>
+      )}
+    </MenuContext.Consumer>
   );
 };
 
